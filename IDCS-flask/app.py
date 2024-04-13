eee6c432bb48ef9c47bb6bd9354114ea7ab9ca43
Saksham <ksaksham39@gmail.com>
2024-04-13 08:02:34 +0530

    init

--- IDCS-flask/app.py
+++ IDCS-flask/app.py
@@ -1,5 +1,5 @@
-import subprocess  # Add this import statement
-
+import os
+import subprocess
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -11,6 +11,18 @@ def execute_python_script():
     script_path = request.json.get('script_path')
     result = subprocess.run(['python', script_path], capture_output=True, text=True)
     return jsonify({'output': result.stdout})
+
+@app.route('/handle_user_input', methods=['POST'])
+def handle_user_input():
+    print("called")
+    data = request.json.get('user_input')
+    file_name = os.path.join(os.path.dirname(__file__), 'script.py')
+    try:
+        with open(file_name, 'w') as f:
+            f.write(data)
+        return jsonify({'message': 'Python script created successfully', 'file_path': file_name})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- IDCS-flask/script.py
+++ IDCS-flask/script.py
@@ -1,1 +1,1 @@
-print("HIIi")
+asdasdasdasd
